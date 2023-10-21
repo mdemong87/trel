e9e5b231c640f84440e52246ed49eda67f16bc38
Md Emon Hossen <mdemong87@gmail.com>
2023-10-21 14:27:56 +0600

    update

--- componnent/pages/Footer.jsx
+++ componnent/pages/Footer.jsx
@@ -1,8 +1,48 @@
 import styles from "../../styles/pages/Footer.module.css";
+import Container from "../Container";
 
 
 export default function Footer() {
     return (
-        <div className={styles.footerWrp}>Footer</div>
+        <div className={styles.footerWrp}>
+            <Container>
+                <div className={styles.footerWrp}>
+                    <div>
+                        gdfgfd
+                    </div>
+                    <div>
+                        <h2>gdfgdsfgsdfg</h2>
+                        <ul>
+                            <li>gfdsgdsfg</li>
+                            <li>gfdsgdsfg</li>
+                            <li>gfdsgdsfg</li>
+                            <li>gfdsgdsfg</li>
+                            <li>gfdsgdsfg</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>gdfgdsfgsdfg</h2>
+                        <ul>
+                            <li>gfdsgdsfg</li>
+                            <li>gfdsgdsfg</li>
+                            <li>gfdsgdsfg</li>
+                            <li>gfdsgdsfg</li>
+                            <li>gfdsgdsfg</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div className={styles.copyRightWrp}>
+                    <div>
+                        gdfg
+                    </div>
+                    <div>fgfg</div>
+                </div>
+            </Container>
+        </div>
     )
 }
+
+
+
+
